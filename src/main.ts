e0f3d3c2b980f45c54bf1058e7374c6749ab9439
AlexValenz <valenzuela1235@gmail.com>
2025-03-16 18:28:18 -0600

    cambio a ip del servidor de aws para permisos del cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200', // Permite solicitudes desde el frontend
+    origin: 'http://18.223.180.64:4200', // Permite solicitudes desde el frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
     credentials: true, // Permite enviar cookies y encabezados de autenticación
   });
