7e93621ad6f3e6ab492fedecf98410ead112d38f
AlexValenz <valenzuela1235@gmail.com>
2025-03-16 19:11:30 -0600

    acceso de control al cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,16 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
     credentials: true, // Permite enviar cookies y encabezados de autenticación
   });
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://frontend-transportes.s3-website.us-east-2.amazonaws.com');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);  // Respondemos a la solicitud preflight
+    }
+    next();
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
