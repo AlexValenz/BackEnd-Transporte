e6aa8f28b3e2c93e56816df518ab4b387e7560f5
AlexValenz <valenzuela1235@gmail.com>
2025-03-16 19:25:14 -0600

    agregar link de amazon

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,7 @@ async function bootstrap() {
     credentials: true, // Permite enviar cookies y encabezados de autenticación
   });
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://frontend-transportes.s3-website.us-east-2.amazonaws.com');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(200);  // Respondemos a la solicitud preflight
-    }
-    next();
-  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
